810b770e2a7c44f400d46d783c67fec370aa58b4
Will Franklin <will.franklin@guardian.co.uk>
2013-08-10 20:41:12 +0100

    Added resources page

---
+++ 2/resources.php
@@ -0,0 +1,25 @@
+<div class="content">
+    <div class="title"><img src="images/resources.png" alt="Resources"></div>
+    <img src="images/books.png" style="float:right; margin:20px" />
+    <p>
+        Following on from <strong>Le Fran&ccedil;ais par les Jeux</strong>, a 
+        unique resource to enable you to teach French to 7-11 year olds with 
+        lots of games and fun activities, <strong>Le Fran&ccedil;ais en 
+        Activites</strong> provides a revision of the words learnt in the first 
+        level, with more words, practised through different activities.
+        <br /><br />
+        There are 27 different topics from everyday life (from numbers and 
+        colours to verbs and adjectives), with 4 activities for each topic. You 
+        will also find master sheets to produce lotto and domino games.
+        <br /><br />
+        Ideal for the French speaker who needs backup material. With the set of 
+        2 books, you have enough material to cover tuition for half to one hour 
+        per week for 4 years.
+        <br /><br />
+        Both books can also be used at KS3 to learnn and revise vocabulary in a 
+        fun and challenging way. Both resources can be purchased on hard or 
+        soft copy for easy duplicating.
+        <br /><br />
+        Order your copy <a href="#">here</a> now.
+    </p>
+</div>
