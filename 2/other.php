5d40ec787b7a01804518fb8028aa3cca32a929d6
Will Franklin <will.franklin@guardian.co.uk>
2013-08-10 21:04:22 +0100

    Added other languages page

---
+++ 2/other.php
@@ -0,0 +1,21 @@
+<div class="content">
+    <div class="title"><img src="images/otherlanguages.png" alt="Other Languages"></div>
+    <div class="tutor">
+        <img class="photo" src="images/tutors/eangeli.png" alt="Elisabetta Angeli" />
+        <div class="description">
+            <img class="title" src="images/tutors/eangeli_title.png" alt="Elisabetta Angeli" />
+            &ldquo;Native Italian teacher, providing high quality Italian 
+            Language services including tuition, translation and interpretation 
+            to individuals and companies.&rdquo;<br />
+            <br />
+            Contact: <a href="mailto:elisbetta@angeliclanguages.co.uk">elisbetta@angeliclanguages.co.uk</a>, phone 01242 690471</br >
+            Website: <a href="http://www.angeliclanguages.co.uk">www.angeliclanguages.co.uk</a>
+        </div>
+    </div>
+    <div class="tutor">
+        <img class="photo" src="images/tutors/nopic.png" alt="Florentia Choo" />
+        <div class="description">
+            <img class="title" src="images/tutors/fchoo_title.png" alt="Florentia Choo" />
+        </div>
+    </div>
+</div>
