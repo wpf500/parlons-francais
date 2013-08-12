bfc5d7dcc86d873e00a918e87f1f0e28b49865f7
Will Franklin <will.franklin@guardian.co.uk>
2013-08-12 23:42:11 +0100

    Fixed testimonials page

--- 2/sections/testimonials.php
+++ 2/sections/testimonials.php
@@ -1,14 +1,14 @@
 <div class="content-header"><img src="static/images/testimonials_page.png" alt="Testimonials" /></div>
 <div class="content">
     <div class="quote">
-        <img class="left" src="static/images/quote_left.png" />
+        <img class="quote-ldquo" src="static/images/quote_left.png" />
         <img class="title" src="static/images/welldone.png" alt="Well done" />
         &hellip; it means she got three A-levels and has been offered a place 
         at xxxxx University. Without your tuition she would definitely have 
         failed her French, and therefore not gone to University - so a HUGE 
         thank you for your help.<br />
         <br />
-        <img class="right" src="static/images/quote_right.png" />
+        <img class="quote-rdquo" src="static/images/quote_right.png" />
         by Sophie1252<br />
         Saturday, September 15 2012
     </div>
