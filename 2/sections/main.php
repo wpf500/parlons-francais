ecd1a51f4bb97aaace4e214dbc35a557233e46cb
Will Franklin <will.franklin@guardian.co.uk>
2013-08-12 23:34:40 +0100

    Added a <p>

--- 2/sections/main.php
+++ 2/sections/main.php
@@ -2,28 +2,32 @@
     <div class="box left">
         <img class="box-logo" src="static/images/aboutus_logo.png" alt="About Us" />
         <img class="box-title" src="static/images/aboutus.png" alt="About Us" />
-        Parlons Fran&ccedil;ais was established by Sophie Franklin in
-        2001. We are now 6 teachers, each with our own
-        specialisms and experiences. We teach from our homes, in
-        Cheltenham, Gloucester and the Cotswolds, or in
-        students&rsquo; homes if they prefer. We also teach in
-        industry and colleges. Our students age from 6 to 70+ and we
-        are committed to satisfying the wishes of every student,
-        from beginner to advanced.
-        <a class="button content-button" href="meetthetutors">Meet The Tutors</a>
+        <p>
+            Parlons Fran&ccedil;ais was established by Sophie Franklin in
+            2001. We are now 6 teachers, each with our own
+            specialisms and experiences. We teach from our homes, in
+            Cheltenham, Gloucester and the Cotswolds, or in
+            students&rsquo; homes if they prefer. We also teach in
+            industry and colleges. Our students age from 6 to 70+ and we
+            are committed to satisfying the wishes of every student,
+            from beginner to advanced.
+            <a class="button content-button" href="meetthetutors">Meet The Tutors</a>
+        </p>
     </div>
     <div class="box right">
         <img class="box-logo" src="static/images/wlwu_logo.png" alt="Why Learn With Us" />
         <img class="box-title" src="static/images/wlwu.png"  alt="Why Learn With Us" />
-        <ul>
-            <li>Planning a holiday in France?</li>
-            <li>About to buy a property in France?</li>
-            <li>Need to revise for your exams?</li>
-            <li>Want the children to feel European?</li>
-            <li>Could do with better French for work?</li>
-            <li>Struggling with French at school?</li>
-        </ul>
-        Whatever you require, contact Parlons Fran&ccedil;ais now.
+        <p>
+            <ul>
+                <li>Planning a holiday in France?</li>
+                <li>About to buy a property in France?</li>
+                <li>Need to revise for your exams?</li>
+                <li>Want the children to feel European?</li>
+                <li>Could do with better French for work?</li>
+                <li>Struggling with French at school?</li>
+            </ul>
+            Whatever you require, contact Parlons Fran&ccedil;ais now.
+        </p>
     </div>
 </div>
 <div class="content">
