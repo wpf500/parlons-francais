20de806f27ceb34ccd485082b156508027e70996
Will Franklin <will.franklin@guardian.co.uk>
2013-08-12 23:35:02 +0100

    Added tutor SMACSS classes

--- 2/sections/other.php
+++ 2/sections/other.php
@@ -1,21 +1,24 @@
 <div class="content-header"><img src="static/images/otherlanguages.png" alt="Other Languages" /></div>
 <div class="content">
     <div class="tutor">
-        <img class="photo" src="static/images/tutors/eangeli.png" alt="Elisabetta Angeli" />
-        <div class="description">
-            <img class="title" src="static/images/tutors/eangeli_title.png" alt="Elisabetta Angeli" />
-            &ldquo;Native Italian teacher, providing high quality Italian 
-            Language services including tuition, translation and interpretation 
-            to individuals and companies.&rdquo;<br />
-            <br />
-            Contact: <a href="mailto:elisbetta@angeliclanguages.co.uk">elisbetta@angeliclanguages.co.uk</a>, phone 01242 690471</br >
-            Website: <a href="http://www.angeliclanguages.co.uk">www.angeliclanguages.co.uk</a>
+        <img class="tutor-photo" src="static/images/tutors/eangeli.png" alt="Elisabetta Angeli" />
+        <div class="tutor-profile">
+            <img class="tutor-profile-title" src="static/images/tutors/eangeli_title.png" alt="Elisabetta Angeli" />
+            <p>
+                &ldquo;Native Italian teacher, providing high quality Italian 
+                Language services including tuition, translation and interpretation 
+                to individuals and companies.&rdquo;
+            </p>
+            <p>
+                Contact: <a href="mailto:elisbetta@angeliclanguages.co.uk">elisbetta@angeliclanguages.co.uk</a>, phone 01242 690471</br >
+                Website: <a href="http://www.angeliclanguages.co.uk">www.angeliclanguages.co.uk</a>
+            </p>
         </div>
     </div>
     <div class="tutor">
-        <img class="photo" src="static/images/tutors/nopic.png" alt="Florentia Choo" />
-        <div class="description">
-            <img class="title" src="static/images/tutors/fchoo_title.png" alt="Florentia Choo" />
+        <img class="tutor-photo" src="static/images/tutors/nopic.png" alt="Florentia Choo" />
+        <div class="tutor-profile">
+            <img class="tutor-profile-title" src="static/images/tutors/fchoo_title.png" alt="Florentia Choo" />
         </div>
     </div>
 </div>
