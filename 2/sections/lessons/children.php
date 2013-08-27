79492114f651d22f268de2a20239359fc6da748d
Will Franklin <will.franklin@guardian.co.uk>
2013-08-28 00:55:38 +0100

    Added children subsection

---
+++ 2/sections/lessons/children.php
@@ -0,0 +1,26 @@
+<div class="content-header"><img src="static/images/children.png" alt="Group Classes for Young Children" /></div>
+<div class="content">
+    <div class="left">
+        <img class="content-title" src="static/images/children_logo.png" alt="Group Classes for Young Children" />
+    </div>
+    <div class="right" style="width: 500px">
+        <p>
+            Classes for KS1 and KS2 children are available on request.<br />
+            The lessons last half an hour and are filled with activities which 
+            are both fun and educational. An exercise book is provided free of 
+            charge, to work on during the class and for homework in between 
+            lessons.
+        </p>
+        <p>
+            The lessons start at &pound;8.00 per week but decrease depending on 
+            the number of students joining the class. Maximum 4 children per 
+            class, at a cost of just &pound;4.00 per child.
+        </p>
+        <p>
+            Bring your friends and it will be cheaper!
+        </p>
+        <p>
+            Please <a href="contact">contact us</a> to book a class.
+        </p>
+    </div>
+</div>
