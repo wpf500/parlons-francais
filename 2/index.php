38442f293f10ab245d754f4e7303cb7e6a196d61
Will Franklin <will.franklin@guardian.co.uk>
2013-08-27 23:35:56 +0100

    Added basics for submenu

--- 2/index.php
+++ 2/index.php
@@ -1,6 +1,16 @@
 <?php
-    $section = $_GET['s'];
-    if (!$section) $section = 'main';
+    $section = $_GET["s"];
+    if (!$section) $section = "main";
+
+    function print_nav($type, $nav) {
+        print("<ul id=\"$type\">");
+        $width = 1 / count($nav) * 100;
+        foreach ($nav as $link => $name) {
+            $current = $GLOBALS["section"] == $link ? " is-current" : "";
+            print("<li class=\"$type-item$current\" style=\"width: $width%\"><a class=\"$type-item-link\" href=\"$link\">$name</a></li>");
+        }
+        print("</ul>");
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -30,24 +40,28 @@
                     and young children
                 </div>
             </div>
-            <ul id="menu">
-                <?php
-                    $nav = array(
-                        "lessons" => "French Lessons",
-                        "library" => "Library",
-                        "resources" => "Resources",
-                        "testimonials" => "Testimonials",
-                        "other" => "Other Languages",
-                        "contact" => "Contact Us"
-                    );
-                    $width = 1 / count($nav) * 100;
-                    foreach ($nav as $link => $name) {
-                        $current = $section == $link ? " is-current" : "";
-                        print("<li class=\"menu-item$current\" style=\"width:$width%\"><a class=\"menu-item-link\" href=\"$link\">$name</a></li>");
-                    }
-                ?>
-            </ul>
-            <?php include("sections/$section.php"); ?>
+            <?php
+                $nav = array(
+                    "lessons" => "French Lessons",
+                    "library" => "Library",
+                    "resources" => "Resources",
+                    "testimonials" => "Testimonials",
+                    "other" => "Other Languages",
+                    "contact" => "Contact Us"
+                );
+                print_nav("menu", $nav);
+
+                $subnav = array(
+                    "children" => "Group Classes for Young Children",
+                    "adults" => "Group Classes for Adults",
+                    "private" => "Private Classes for Adults &amp; Secondary Students",
+                    "internet" => "Internet Teaching",
+                    "business" => "Business French"
+                );
+                print_nav("submenu", $subnav);
+
+                include("sections/$section.php");
+            ?>
             <div style="clear:both"></div>
         </div>
     </body>
