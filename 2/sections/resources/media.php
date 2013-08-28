0e6ea6cd0ecc743036ac6a5a6d47f9dd003f21f7
Will Franklin <will.franklin@guardian.co.uk>
2013-08-28 22:52:21 +0100

    Added resources media page

---
+++ 2/sections/resources/media.php
@@ -0,0 +1,9 @@
+<div class="content-header"><img src="static/images/media.png" alt="International French TV, French TV Portals, French Radio, French Newspapers" /></div>
+<div class="content">
+    <p>
+        Useful resources to practise your French reading and listening between 
+        lessons, or simply keep up-to-date with French current affairs and 
+        culture.
+    </p>
+    <img src="static/images/media_logo.png" />
+</div>
