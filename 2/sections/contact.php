36238fab59dc33be6f88447826bbba5b8661f487
Will Franklin <will.franklin@guardian.co.uk>
2013-08-29 00:14:43 +0100

    Added mail handler

--- 2/sections/contact.php
+++ 2/sections/contact.php
@@ -1,12 +1,42 @@
+<?php
+    $message = $_POST['message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    if ($message && $name && $email) {
+        $to = "info@parlons-francais.co.uk";
+        $subject = "Parlons Français web enquiry from $name";
+        $message = "This enquiry is from $name ($email) via the Parlons Français web site.\r\n".
+         "=======================================\r\n\r\n$message";
+        mail($to, $subject, $message, "Reply-To: $email");
+
+        $subject = "Automatic message acknowledgement";
+        $message = "The following message has been sent from you to Parlons Français.\r\n".
+        "(This acknowledgement is from an automatic service that does not accept replies).\r\n\r\n".$mesage;
+        mail ($email, $subject, $message);
+
+        $sent = true;
+    }
+?>
 <div class="content-header"><img src="static/images/contactus.png" alt="Contact Us" /></div>
 <div class="content">
     <div class="left">
+        <?php
+            if ($sent) {
+        ?>
+        <p>Thank you for your message.</p>
+        <p>We will reply as soon as possible.</p>
+        <?php
+            } else {
+        ?>
         <form method="POST" id="contact-form">
             <textarea id="contact-form-message" placeholder="Write your message here" name="message" required></textarea>
             <label for="contact-form-name">Your name:</label><input id="contact-form-name" type="text" name="name" required />
             <label for="contact-form-email">Your e-mail address:</label><input id="contact-form-email" type="email" name="email" required />
             <input type="submit" value="Send" />
         </form>
+        <?php
+            }
+        ?>
     </div>
     <div class="right">
         <div class="contact-info">
