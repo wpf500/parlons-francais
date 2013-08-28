57c99c6c9ce950d33c1e67472bdfc54ed2be6098
Will Franklin <will.franklin@guardian.co.uk>
2013-08-28 22:10:29 +0100

    Added internet section

---
+++ 2/sections/lessons/internet.php
@@ -0,0 +1,38 @@
+<div class="content-header"><img src="static/images/internet.png" alt="Internet Teaching" /></div>
+<div class="content">
+    <div class="left" style="width: 500px">
+        <p>
+            Now you don't need to live in the same town as your tutor. Thanks 
+            to the internet, you can book your class, AND have your lesson 
+            online wherever you are! All you need is a headset and Skype, which 
+            is free to download <a href="http://www.skype.com">here</a>. If you 
+            haev a webcome, you will be able to see the tutor and vice-versa. 
+            Tuition is specially designed to suit each individual's 
+            requirements.
+        </p>
+        <p>
+            The same rates apply as our One-To-One classes, but you save on 
+            travelling time and expenses.
+        </p>
+        <p>
+            <strong>Adults:</strong> &pound;23.00 per hour<br />
+            <strong>Children up to GCSE level:</strong> &pound;20.00 per hour<br />
+            <strong>Perparation for AS and A2 exams:</strong> &pound;23.00 per hour<br />
+        </p>
+        <p>
+            A generous discount is applicable for advance block payments of 10 
+            sessions.
+        </p>
+        <p>
+            Online classes are all taught by French Nationals - <a 
+            href="meetthetutors">Meet the Tutors</a>
+        </p>
+        <p>
+            Please <a href="contact">contact us</a> for further details and to 
+            book a class.
+        </p>
+    </div>
+    <div class="right">
+        <img class="content-title" src="static/images/internet_logo.png" alt="Internet Teaching" />
+    </div>
+</div>
