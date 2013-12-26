60464ce74c09fdfef3c4b871070e83660776223c
Will Franklin <will.franklin@guardian.co.uk>
2013-12-26 15:30:12 +0000

    New testimonials

--- 2/sections/home.php
+++ 2/sections/home.php
@@ -35,12 +35,9 @@
     <img class="content-title" src="static/images/testimonials.png" alt="Testimonials" />
     <div class="quote left">
         <img class="quote-ldquo" src="static/images/quote_left.png" />
-        <img class="quote-title" src="static/images/welldone.png" alt="Well done" />
-        &hellip; it means she got three A-levels and has been offered a place 
-        at University. Without your tuition she would definitely have 
-        failed her French, and therefore not gone to University - so a HUGE 
-        thank you for your help.<br /><br />
-        - P.Smith
+        I've enjoyed stretching my brain in such good company and wish you all 
+        the best for the future, thanks for everything. Thanks Anne for everything<br /><br />
+        - Catherine
         <img class="quote-rdquo" src="static/images/quote_right.png" />
     </div>
     <div class="quote right">
--- 2/sections/testimonials.php
+++ 2/sections/testimonials.php
@@ -2,6 +2,24 @@
 <div class="content">
     <div class="quote left">
         <img class="quote-ldquo" src="static/images/quote_left.png" />
+        I've enjoyed stretching my brain in such good company and wish you all 
+        the best for the future, thanks for everything. Thanks Anne for everything<br /><br />
+        - Catherine
+        <img class="quote-rdquo" src="static/images/quote_right.png" />
+    </div>
+    <div class="quote right">
+        <img class="quote-ldquo" src="static/images/quote_left.png" />
+        Thank you Sophie for all of the lessons we had together which I 
+        thoroughly enjoyed. Your teaching greatly increased my overall 
+        confidence and I was very able to converse with my colleagues in French 
+        whilst in Morocco!<br /><br />
+        - Christina
+        <img class="quote-rdquo" src="static/images/quote_right.png" />
+    </div>
+</div>
+<div class="content">
+    <div class="quote left">
+        <img class="quote-ldquo" src="static/images/quote_left.png" />
         <img class="quote-title" src="static/images/welldone.png" alt="Well done" />
         &hellip; it means she got three A-levels and has been offered a place 
         at University. Without your tuition she would definitely have 
