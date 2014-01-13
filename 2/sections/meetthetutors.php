48eec2713ee3c013a130acb8f877ed29ab89df9a
Will Franklin <will.franklin@guardian.co.uk>
2014-01-13 00:28:22 +0000

    Added Josette

--- 2/sections/meetthetutors.php
+++ 2/sections/meetthetutors.php
@@ -35,4 +35,12 @@
             <?=content("meetthetutors", "msummersgill")?>
         </div>
     </div>
+    <div class="tutor">
+        <img class="tutor-photo" src="static/images/tutors/josette.jpg" alt="Josette" />
+        <div class="tutor-profile">
+            <!--<img class="tutor-profile-title" src="static/images/tutors/josette_title.png" alt="Josette" />-->
+            <b>Josette</b>
+            <?=content("meetthetutors", "josette")?>
+        </div>
+    </div>
 </div>
