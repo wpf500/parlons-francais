8813a91f71b83b895dd9e22bb717b566e6636a52
Will Franklin <will.franklin@guardian.co.uk>
2014-03-29 13:57:28 +0000

    No need to have hidden inputs

--- 2/index.php
+++ 2/index.php
@@ -42,8 +42,6 @@
 
         if ($GLOBALS["admin"]) {
             print "<form method=\"POST\">";
-            print "<input type=\"hidden\" name=\"section\" value=\"$section\" />";
-            print "<input type=\"hidden\" name=\"subsection\" value=\"$subsection\" />";
             print "<textarea name=\"body\" class=\"admin\">$body</textarea>";
             print "<button>Save</button>";
             print "</form>";
@@ -54,8 +52,8 @@
 
     $body = mysql_escape_string($_POST["body"]);
     if ($body && $admin) {
-        $q = "UPDATE content SET body='$body' WHERE section='$_POST[section]'";
-        $q .= " AND subsection='$_POST[subsection]'";
+        $q = "UPDATE content SET body='$body' WHERE section='$section'";
+        $q .= " AND subsection='$subsection'";
         mysql_query($q);
     }
 ?>
