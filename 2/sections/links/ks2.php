252ae35fdf082765d8c2fec5fca0aca593498d28
Will Franklin <will.franklin@guardian.co.uk>
2014-03-29 14:33:20 +0000

    Change section

--- 2/sections/links/ks2.php
+++ 2/sections/links/ks2.php
@@ -1,7 +1,7 @@
 <div class="content-header"><img src="static/images/resources.png" alt="Resources" /></div>
 <div class="content">
     <div class="left" style="width: 500px">
-        <?=content("resources", "ks2")?>
+        <?=content("links", "ks2")?>
     </div>
     <div class="right">
         <a href="http://www.classroom-resources.co.uk/acatalog/Online_Catalogue_Le_Fran_ais_par_les_Jeux___Part_1_538.html" target="_blank">
