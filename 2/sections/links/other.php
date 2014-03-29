f375c889ab7b684f8c08ca26dcd4c3847fb6fd08
Will Franklin <will.franklin@guardian.co.uk>
2014-03-29 14:55:58 +0000

    Added back fschoo

--- 2/sections/links/other.php
+++ 2/sections/links/other.php
@@ -4,21 +4,14 @@
         <img class="tutor-photo" src="static/images/tutors/eangeli.png" alt="Elisabetta Angeli" />
         <div class="tutor-profile">
             <img class="tutor-profile-title" src="static/images/tutors/eangeli_title.png" alt="Elisabetta Angeli" />
-            <p>
-                &ldquo;Native Italian teacher, providing high quality Italian 
-                Language services including tuition, translation and interpretation 
-                to individuals and companies.&rdquo;
-            </p>
-            <p>
-                Contact: <a href="mailto:elisbetta@angeliclanguages.co.uk">elisbetta@angeliclanguages.co.uk</a>, phone 01242 690471</br >
-                Website: <a href="http://www.angeliclanguages.co.uk">www.angeliclanguages.co.uk</a>
-            </p>
+            <?=content("links-other", "eangeli")?>
         </div>
     </div>
-<!--<div class="tutor">
+    <div class="tutor">
         <img class="tutor-photo" src="static/images/blank.gif" alt="Florencia Schoo" />
         <div class="tutor-profile">
             <img class="tutor-profile-title" src="static/images/tutors/fschoo_title.png" alt="Florencia Schoo" />
+            <?=content("links-other", "fschoo")?>
         </div>
-    </div>-->
+    </div>
 </div>
